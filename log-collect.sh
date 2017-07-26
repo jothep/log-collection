0fb2675e9700a3e5281f2efc1df17ce3f44f4960
jothep <190911232@qq.com>
2017-07-26 15:21:22 +0800

    Create log-collect.sh

---
+++ log-collect.sh
@@ -0,0 +1,13 @@
+# this shell script is put in image: jasko/log-collect
+# this script use for synchronize logfiles from original path to another place, and print out a time message to show the synchonize is keeping
+# options use environments
+# $HOSTNAME: podname ; $LOGOPT: rsync options ; $LOG_LOCATION: original log path ; $INTERVAL: sync per every time
+
+#!/bin/bash
+mkdir -p /mnt/$HOSTNAME
+while true; do
+  rsync $LOGOPT $LOG_LOCATION /mnt/$HOSTNAME
+  T=$(date)
+  echo $T " [log collected]"
+  sleep $INTERVAL
+done
